f33b29d989e193386f8c7c21bf281cae3e63f5b9
iZume <adler32mail@gmail.com>
2016-08-14 18:46:40 -0600

    Updated files

--- client-samp/includes/data.php
+++ client-samp/includes/data.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+
 require('./monitor.php');
 
 if(isset($_GET['ip']) && isset($_GET['data'])) 
--- client-samp/js/samp-client.js
+++ client-samp/js/samp-client.js
@@ -1,100 +1,60 @@
-	$(function(){
-		var lastBlock = 0;
-		loadInternetList();
-		$(document).on('click', '.windows-client tbody > tr', function(){
-    		$('.windows-client tbody > tr').removeClass('sombrear');
+/*
+  * Autor: Zume (http://forum.sa-mp.com/member.php?u=146395)
+*/
 
-		    $(this).toggleClass("sombrear");
-		})
-		$(document).on('click', '.servergetid', function() {
-			$('#pd').html('');
-			$('#prop').html('');
+$(function() {
+    function b() {
+        $.getJSON("http://nextgaming.net/samp-client/client-samp/includes/master-list.php?q=master", function(a) {
+            $.each(a, function(a, b) {
+                c(b)
+            })
+        })
+    }
 
-			var k = $(this),
-				lst = false
-			;
+    function c(a) {
+        var b = !1;
+        return $.getJSON("http://nextgaming.net/samp-client/client-samp/includes/data.php?ip=" + a + "&data=info", function(a) {
+            b = d(a, "[x]")
+        }), b
+    }
 
-			$.ajax({
-			  url: './client-samp/includes/data.php?ip=' + $(this).data('server') + '&data=l_by_id',
-			  dataType: 'json',
-			  type: 'GET',
-			  async: false,
-			  success: function(data) {
-			  	var prp = [
-					{'rule':'lagcomp', 	'value':'true'},
-					{'rule':'mapname', 	'value':data.info.Map},
-					{'rule':'version', 	'value':data.info.Version},
-					{'rule':'weather', 	'value':'10'},
-					{'rule':'weburl', 	'value':data.info.WebURL},
-					{'rule':'worldtime','value':data.info.Time}
-				];
-			  	$.each(prp, function(key, item) {
-			  		$('#prop').append('<tr ' + (lst == true ? 'class="black"' : '') + '><td>' + item.rule + '</td><td>' + item.value + '</td></tr>');
-			  		lst = lst == true ? false : true;
-			  	}); 
+    function d(b, c) {
+        if ("undefined" === b.Gamemode) return !1;
+        var d = "",
+            f = 1 == a ? "black" : "";
+        return d = d + '<tr id="d_' + b.ServerID + '" class="servergetid ' + f + '" data-server="' + b.ServerID + '">', d = d + '<td id="password" class="icon-password">' + e(b.Password) + "</td>", d = d + '<td id="hostname">' + b.Hostname + "</td>", d = d + '<td id="players">' + b.Players + "/" + b.MaxPlayers + "</td>", d = d + '<td id="ping">' + c + "</td>", d = d + '<td id="gamemode">' + b.Gamemode.substring(0, 20) + "</td>", d = d + '<td id="map">' + b.Map.substring(0, 15) + "</td>", d += "</tr>", a = 1 == a ? 0 : 1, $("#sl05").append(d), !0
+    }
 
-			  	k.children('td#hostname').html(data.info.Hostname);
-			  	k.children('td#password').html(getServerStatus(data.info.Password));
-			  	k.children('td#players').html(data.info.Players + '/' + data.info.MaxPlayers);
-			  	k.children('td#ping').html('[x]');
-			  	k.children('td#gamemode').html(data.info.Gamemode.substring(0, 20));
-			  	k.children('td#map').html(data.info.Map.substring(0, 15));
-			  		
-			  	$('#server-info-bottom').html(data.info.IP + ':' + data.info.Port);
-			  	$('#server-web-url').html('<a href="' + data.info.WebURL + '">' + data.info.WebURL + '</a>');
-
-				lst = false;
-
-			  	$.each(data.players, function(i, item) {
-					$('#pd').append('<tr ' + (lst == true ? 'class="black"' : '') + '><td>' + item.Nickname + '</td><td>' + item.Score + '</td></tr>');
-					lst = lst == true ? false : true;
-				});
-
-			  }
-			});
-		});
-		function loadInternetList()
-		{
-			$.getJSON('./client-samp/includes/master-list.php?q=master', function(data) {
-				$.each(data, function(i, item) {
-					addServer(item);
-				});
-			});
-		}
-		function addServer(ip)
-		{
-			var response = false;
-			$.getJSON('./client-samp/includes/data.php?ip=' + ip + '&data=info', function(data)
-			{
-				response = addServerInfo(data, '[x]');
-			});
-			return response;
-		}
-		function addServerInfo(data, ping)
-		{
-			if(data.Gamemode === 'undefined')
-				return false;
-
-			var out = '';
-
-			var block = lastBlock == 1 ? 'black' : '';
-
-			out = out + '<tr id="d_' + data.ServerID + '" class="servergetid ' + block + '" data-server="' + data.ServerID + '">';
-			out = out + '<td id="password" class="icon-password">' + getServerStatus(data.Password) + '</td>';
-			out = out + '<td id="hostname">' + data.Hostname + '</td>';
-			out = out + '<td id="players">' + data.Players + '/' + data.MaxPlayers + '</td>';
-			out = out + '<td id="ping">' + ping + '</td>';
-			out = out + '<td id="gamemode">' + data.Gamemode.substring(0, 20) + '</td>';
-			out = out + '<td id="map">' + data.Map.substring(0, 15) + '</td>';
-			out = out + '</tr>';
-
-			lastBlock = lastBlock == 1 ? 0 : 1;
-
-			$('#sl05').append(out);
-			return true;	
-		}
-		function getServerStatus(lock)
-		{
-			return (lock == 0 ? '<img src="client-samp/images/server-unlocked.jpg" alt="+"></img>' : '<img src="client-samp/images/server-locked.jpg" alt="-"></img>');
-		}
-	});
+    function e(a) {
+        return 0 == a ? '<img src="client-samp/images/server-unlocked.jpg" alt="+"></img>' : '<img src="client-samp/images/server-locked.jpg" alt="-"></img>'
+    }
+    var a = 0;
+    b(), $(document).on("click", ".windows-client tbody > tr", function() {
+        $(".windows-client tbody > tr").removeClass("sombrear"), $(this).toggleClass("sombrear")
+    }), $(document).on("click", ".servergetid", function() {
+        $("#pd").html(""), $("#prop").html("");
+        var a = $(this),
+            b = !1;
+        $.ajax({
+            url: "http://nextgaming.net/samp-client/client-samp/includes/data.php?ip=" + $(this).data("server") + "&data=l_by_id",
+            dataType: "json",
+            type: "GET",
+            async: !1,
+            success: function(c) {
+                var d = [
+                	{rule: "lagcomp", value: "true"}, 
+	                {rule: "mapname", value: c.info.Map}, 
+	                {rule: "version", value: c.info.Version}, 
+	                {rule: "weather", value: "10"}, 
+	                {rule: "weburl", value: c.info.WebURL}, 
+	                {rule: "worldtime", value: c.info.Time}
+                ];
+                $.each(d, function(a, c) {
+                    $("#prop").append("<tr " + (1 == b ? 'class="black"' : "") + "><td>" + c.rule + "</td><td>" + c.value + "</td></tr>"), b = 1 != b
+                }), a.children("td#hostname").html(c.info.Hostname), a.children("td#password").html(e(c.info.Password)), a.children("td#players").html(c.info.Players + "/" + c.info.MaxPlayers), a.children("td#ping").html("[x]"), a.children("td#gamemode").html(c.info.Gamemode.substring(0, 20)), a.children("td#map").html(c.info.Map.substring(0, 15)), $("#server-info-bottom").html(c.info.IP + ":" + c.info.Port), $("#server-web-url").html('<a href="' + c.info.WebURL + '">' + c.info.WebURL + "</a>"), b = !1, $.each(c.players, function(a, c) {
+                    $("#pd").append("<tr " + (1 == b ? 'class="black"' : "") + "><td>" + c.Nickname + "</td><td>" + c.Score + "</td></tr>"), b = 1 != b
+                })
+            }
+        })
+    })
+});
